d54b7809752b9213aeed803f5eb7dedaa2b6509f
luke-boylan <boylanlu@tcd.ie>
2025-09-01 17:04:11 +0100

    apply md-styles change

--- src/app/Components/ProjectBox/ProjectBox.tsx
+++ src/app/Components/ProjectBox/ProjectBox.tsx
@@ -60,7 +60,7 @@ const ProjectBox = ({title, image, description, link}: {title:string, image:stri
                     </Dialog>
                     <Dialog open={videoOpen} onClose={() => setVideoOpen(false)} className="relative z-50">
                         <div className="fixed inset-0 flex items-center justify-center p-4 backdrop-brightness-75 backdrop-blur-md">
-                            <DialogPanel className="bg-white ">
+                            <DialogPanel className="bg-white md:scale-80">
                                 <video controls className="w-300 h-full">
                                     <source src="Project demonstration video.mp4" type="video/mp4"></source>
                                 </video>
